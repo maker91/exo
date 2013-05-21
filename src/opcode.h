59549e1e3f84e7ad8cbe01fce7c0dd649a3fab82
Matthew Reckless <matthewreckless@hotmail.co.uk>
2013-05-21 11:17:40 +0100

    Added MAKE_ABC and MAKE_ABx macros

--- src/opcode.h
+++ src/opcode.h
@@ -21,6 +21,9 @@
 #define IS_CK(i)	(i>>8) & 0x01
 #define IS_BxS(i)	(i>>16) & 0x01
 
+#define MAKE_ABC(i, A, bk, B, ck, C)	((i&0x3F)<<26)&((A&0xFF)<<18)&((bk&0x01)<<17)&((B&0xFF)<<9)&((ck&0x01)<<8)&(C&0xFF)
+#define MAKE_ABx(i, A, bs, B)			((i&0x3F)<<26)&((A&0xFF)<<18)&((bs&0x01)<<17)&(B&0xFFFF)			
+
 namespace exo {
 	typedef std::uint32_t instruction;
 
