0ee38a89d3565e082264f389af1cb0d60d1d83ae
Matthew Reckless <matthewreckless@hotmail.co.uk>
2013-05-27 21:06:07 +0100

    Added Token enum

---
+++ src/token.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace exo {
+	namespace tokens {
+	
+		enum Token {
+			INVALID,
+			GLOBAL,			// global
+			IDENTIFIER,		// [_a-zA-Z][_0-9a-zA-Z]+
+			SEPARATOR,		// ,
+			ASSIGNMENT,		// =
+			
+			NUMBER,			// [-+]?[0-9]*.?[0-9]+[eE]?[0-9]+
+			INTEGER,		// [+-]?[0-9]+[eE]?[0-9]+
+			STRING,			// ".+"
+			CHAR,			// '.'
+			BOOLEAN,		// true/false
+			FUNCTION,		// function
+			
+			RETURN,			// return
+			
+			LINDEX,			// [
+			RINDEX,			// ]
+			LBRACE,			// {
+			RBRACE,			// }
+			LPAREN,			// (
+			RPAREN,			// )
+			
+			ACCESS,			// .
+			
+			ADD,			// +
+			SUB,			// -
+			MUL,			// *
+			DIV,			// /
+			POW,			// ^
+			MOD,			// %
+			LEN,			// #
+			CONCAT,			// ..
+			
+			UNM,			// -
+			
+			EQUAL,			// ==
+			LT,				// <
+			LE,				// <=
+			GT,				// >
+			GE,				// >=
+			NOTEQL,			// !=
+			
+			NOT,			// !
+			AND,			// &&
+			OR,				// ||
+			
+			BNOT,			// ~
+			BAND,			// &
+			BOR,			// |
+			BXOR,			// @
+		};
+	}
+}
